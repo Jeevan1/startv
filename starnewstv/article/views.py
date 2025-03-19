bc7f76495876edf306943070da3cf0f7307bc4a6
Jeevan <shresthaj1986@gmail.com>
2025-03-19 19:42:54 +0545

    Added permissions

--- starnewstv/article/views.py
+++ starnewstv/article/views.py
@@ -1,16 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from article.pagination import DefaultPagination
+from article.permissions import IsAdmin, IsAdminOrAuthor
 from .filters import ArticleFilter, MenuItemFilter
+from rest_framework.mixins import  CreateModelMixin
 from utils.api_mixins import BaseAPIMixin
+from rest_framework.permissions import AllowAny
 from article.models import Article, Author, Category, Contact, MenuItem
 from article.serializers import ArticleCreateSerializer, ArticleDetailSerializer, ArticleSerializer, AuthorSerializer, CategorySerializer, ContactSerializer, MenuItemsSerializer
-# Create your views here.
-
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import MenuItem
@@ -21,6 +22,12 @@ class MenuItemsViewSet( BaseAPIMixin,ModelViewSet):
     serializer_class = MenuItemsSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = MenuItemFilter
+
+    def get_permissions(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return [IsAdmin()]
+
+        return super().get_permissions()
 
     def list(self, request, *args, **kwargs):
         grouped_menus = {
@@ -42,9 +49,21 @@ class CategoryViewSet(BaseAPIMixin,ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def get_permissions(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return [IsAdmin()]
+
+        return super().get_permissions()
+
 class AuthorViewSet(BaseAPIMixin,ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+    def get_permissions(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return [IsAdmin()]
+
+        return super().get_permissions()
 
 class ArticleViewSet(BaseAPIMixin, ModelViewSet):
     queryset = Article.objects.select_related("author", "category").all()
@@ -54,6 +73,15 @@ class ArticleViewSet(BaseAPIMixin, ModelViewSet):
     search_fields = ["title", "content"]
     pagination_class = DefaultPagination
     ordering_fields = ["created_at"]
+
+    permission_classes = [AllowAny]
+
+    def get_permissions(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return [IsAdminOrAuthor()]
+
+        return super().get_permissions()
+        
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -65,6 +93,12 @@ class ArticleViewSet(BaseAPIMixin, ModelViewSet):
         
         return ArticleSerializer
     
-class ContactViewSet(BaseAPIMixin,ModelViewSet):
+    def perform_create(self, serializer):
+      if self.request.user.is_anonymous:
+         raise PermissionDenied("User is not authenticated")
+      serializer.save(owner=self.request.user)
+    
+class ContactViewSet(BaseAPIMixin, CreateModelMixin, GenericViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
